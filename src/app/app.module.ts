9ed1bfc492924d47fcbecc3f014f20115f38e49d
Dennis HG <dennishg32@gmail.com>
2020-11-12 23:44:25 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { ReposComponent } from './repos/repos.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { RepoDatePipe } from './repo-date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ReposComponent,
+    NavbarComponent,
+    SearchFormComponent,
+    RepoDatePipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/repo-date.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RepoDatePipe } from './repo-date.pipe';
+
+describe('RepoDatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new RepoDatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
