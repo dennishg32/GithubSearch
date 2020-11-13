d321960dc9401af9a6acae37f8b37dd23ed033ed
Dennis HG <dennishg32@gmail.com>
2020-11-13 22:57:28 +0200

    done with github service

--- src/app/github-service/github.service.ts
+++ src/app/github-service/github.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Repository } from '../repository';
 import { User } from '../user';
 import { HttpClient } from '@angular/common/http';
+import { resolve } from 'dns';
+import { rejects } from 'assert';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,58 @@ export class GithubService {
   constructor(private http: HttpClient) {
     this.findUser = new User('', '', '', 0, 0, 0, '', new Date());
     this.repos = new Repository('', '', '', '', new Date());
-   }
+  }
+  
+  searchUser(searchUsername: string) {
+    interface apiResponse {
+      url: string;
+      login: string;
+      location: string;
+      pub_repos: number;
+      followers: number;
+      following: number;
+      avatar_url: string;
+      createdDate: Date;
+    }
+    return new Promise((resolve, reject) => {
+      this.http.get<apiResponse>('https://api.github.com/users/' + searchUsername + '?access_token=' + environment.apiKey).toPromise().then(
+        (response) => {
+          this.findUser = response;
+          console.log(this.findUser);
+          resolve();
+        },
+        (err) => {
+          console.log(err);
+          reject();
+        }
+      );
+    });
+  }
+
+  getRepository(searchRepo: string) {
+
+    interface apiResponse{
+      url: string;
+      description: string;
+      language: string;
+      html_url: string;
+      updated_at: Date;
+    }
+
+    return new Promise((resolve, reject)=>{
+      this.http.get<apiResponse>('https://api.github.com/users/' + searchRepo +
+        '/repos?order=created&sort=asc?access_token=' + environment.apiKey)
+        .toPromise().then(
+        (response) => {
+          this.repos = response;
+          console.log(this.repos);
+          resolve();
+        },
+        (err) => {
+          console.log(err);
+          reject();
+        }
+      );
+    });
+  }
 }
--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,8 +1,8 @@
 export class Repository {
   constructor(
     public url: string,
-    public language: string,
     public description: string,
+    public language: string,
     public html_url: string,
     public updated_at: Date
   ){}
