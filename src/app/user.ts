8896d947da0ed93471dc2b0993c570018157e7bb
Dennis HG <dennishg32@gmail.com>
2020-11-13 19:48:44 +0200

    add users' attributes

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,13 @@
 export class User {
+  constructor(
+    public url: string,
+    public html_url: string,
+    public login: string,
+    public location: string,
+    public pub_repos: number,
+    public following: number,
+    public followers: number,
+    public createdDate: Date,
+    public avatar_url: string
+  ){}
 }
