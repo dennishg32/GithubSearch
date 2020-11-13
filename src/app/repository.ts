a5dc4039e8af4ec8fb695c88bac83494dc06d4b4
Dennis HG <dennishg32@gmail.com>
2020-11-13 20:06:42 +0200

    add repos' attributes

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,9 @@
 export class Repository {
+  constructor(
+    public url: string,
+    public language: string,
+    public description: string,
+    public html_url: string,
+    public updated_at: Date
+  ){}
 }
