193bb1a3477ec93a324fd79de7f1fd2b12e2e994
Dennis HG <dennishg32@gmail.com>
2020-11-14 09:21:56 +0200

    adds searchForm function

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-form',
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
+  searchEngine: string;
+  @Output() searched = new EventEmitter<any>();
 
+  getsearched() {
+    this.searched.emit(this.searchEngine);
+    this.searchEngine = '';
+  }
   constructor() { }
 
   ngOnInit(): void {
