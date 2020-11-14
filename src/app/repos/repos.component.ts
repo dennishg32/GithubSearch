fe7a62080b6d46d7dcbce93c519bdaf07dce725b
Dennis HG <dennishg32@gmail.com>
2020-11-14 09:09:26 +0200

    add repo function in component

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github-service/github.service';
+import { Repository } from '../repository';
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.css']
+  styleUrls: ['./repos.component.css'],
 })
 export class ReposComponent implements OnInit {
+  rep: Repository;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  reposearch(engineSearch) {
+    this.searchRepo.getRepository(engineSearch).then(
+      (grant) => {
+        this.rep = this.searchRepo.repos;
+        console.log(this.rep);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
-
+  constructor(public searchRepo: GithubService) {}
+  ngOnInit(): void {
+    this.reposearch('dennishg32');
+  }
 }
